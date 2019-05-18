98917023dfd9a955523567ac5664b03035e9b494
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-17 23:41:04 -0700

    Move divideHex to own file

---
+++ divide_hex.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strconv"
+)
+
+// Divide two base-16 numbers.
+func divideHex(hexNumerator string, hexDenominator string) float64 {
+	// If either hex value is larger than the maximum allowed
+	// ("FFFFFFFFFFFFFFFF"), ParseUint will panic.
+	intDenominator, err := strconv.ParseUint(hexDenominator, 16, 64)
+	if err != nil {
+		panic(err)
+	}
+	intNumerator, err := strconv.ParseUint(hexNumerator, 16, 64)
+	if err != nil {
+		panic(err)
+	}
+	fraction := float64(intNumerator) / float64(intDenominator)
+	return fraction
+}
--- random.go
+++ random.go
@@ -2,26 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	fraction := divideHex("AAFFFFFFFFFFFFFF", "FFFFFFFFFFFFFFFF")
+	fraction := divideHex("AAAFFFFFFFFFFFFF", "FFFFFFFFFFFFFFFF")
 	fmt.Println(fraction)
-}
-
-// Divide two base-16 numbers.
-func divideHex(hexNumerator string, hexDenominator string) float64 {
-	// If either hex value is larger than the maximum allowed
-	// ("FFFFFFFFFFFFFFFF"), ParseUint will panic.
-	intDenominator, err := strconv.ParseUint(hexDenominator, 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	intNumerator, err := strconv.ParseUint(hexNumerator, 16, 64)
-	if err != nil {
-		panic(err)
-	}
-	fraction := float64(intNumerator) / float64(intDenominator)
-	return fraction
 }
