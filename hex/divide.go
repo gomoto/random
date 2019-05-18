9ff095b8e18cd9fc1a14ad88156291a78b9f0b2b
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-18 00:43:01 -0700

    Return error, don't panic

--- hex/divide.go
+++ hex/divide.go
@@ -5,17 +5,17 @@ import (
 )
 
 // Divide two base-16 numbers.
-func Divide(hexNumerator string, hexDenominator string) float64 {
+func Divide(hexNumerator string, hexDenominator string) (float64, error) {
 	// If either hex value is larger than the maximum allowed
 	// ("FFFFFFFFFFFFFFFF"), ParseUint will panic.
 	intDenominator, err := strconv.ParseUint(hexDenominator, 16, 64)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 	intNumerator, err := strconv.ParseUint(hexNumerator, 16, 64)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 	fraction := float64(intNumerator) / float64(intDenominator)
-	return fraction
+	return fraction, nil
 }
