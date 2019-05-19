f7d7c2a82ce4f6da5ba3538655fd1067bf593934
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-18 22:15:45 -0700

    Parse command-line args

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/big"
 
 	"github.com/gomoto/random/numbers"
 )
 
+// Print random number between specified min and max
 func main() {
+	toRangeMin := flag.Int64("min", 0, "Minimum integer (default 0)")
+	toRangeMax := flag.Int64("max", 0, "Maximum integer (default 0)")
+	flag.Parse()
 	hexValue := "C60BF83BDCBAD2C73F76B5AAC4604603EB325D690969545A28EAD1880A159C8EBEE3E1647957CC9F7AB16F4E7457B40B95F86497A9AEE59BFAFCEFD660927235"
 	hexMin := "0"
 	hexMax := "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
@@ -17,11 +22,8 @@ func main() {
 	intValue.SetString(hexValue, 16)
 	intMin.SetString(hexMin, 16)
 	intMax.SetString(hexMax, 16)
-	fmt.Println(intValue)
-	fmt.Println(intMin)
-	fmt.Println(intMax)
 	fromRange := numbers.IntRange{Min: intMin, Max: intMax}
-	toRange := numbers.IntRange{Min: big.NewInt(1), Max: big.NewInt(49)}
+	toRange := numbers.IntRange{Min: big.NewInt(*toRangeMin), Max: big.NewInt(*toRangeMax)}
 	outputValue, _ := numbers.MapInt(*intValue, fromRange, toRange)
 	fmt.Println(outputValue)
 }
