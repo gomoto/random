1770dc7474439aa9289dc80cac73ec33948af3bf
Ryan Gomoto <ryan@cyrusbio.com>
2019-05-18 21:01:09 -0700

    Transform hex value

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 )
 
 func main() {
-	inputValue := big.NewInt(3)
-	fromRange := numbers.IntRange{Min: big.NewInt(2), Max: big.NewInt(4)}
-	toRange := numbers.IntRange{Min: big.NewInt(10), Max: big.NewInt(19)}
-	outputValue, err := numbers.MapInt(*inputValue, fromRange, toRange)
+	hexValue := "C60BF83BDCBAD2C73F76B5AAC4604603EB325D690969545A28EAD1880A159C8EBEE3E1647957CC9F7AB16F4E7457B40B95F86497A9AEE59BFAFCEFD660927235"
+	hexMin := "0"
+	hexMax := "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+	intValue := new(big.Int)
+	intMin := new(big.Int)
+	intMax := new(big.Int)
+	intValue.SetString(hexValue, 16)
+	intMin.SetString(hexMin, 16)
+	intMax.SetString(hexMax, 16)
+	fmt.Println(intValue)
+	fmt.Println(intMin)
+	fmt.Println(intMax)
+	fromRange := numbers.IntRange{Min: intMin, Max: intMax}
+	toRange := numbers.IntRange{Min: big.NewInt(1), Max: big.NewInt(49)}
+	outputValue, _ := numbers.MapInt(*intValue, fromRange, toRange)
 	fmt.Println(outputValue)
-	fmt.Println(err)
 }
